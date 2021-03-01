6fab682c736feb17fea5f6ce2ef7dda661ae597c
Baptiste Momut <baptiste.momut.etu@univ-lille.fr>
2021-03-01 16:14:01 +0100

    ajout de jeux

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import GameThumbnail from './components/GameThumbnail';
 
-const body = document.querySelector('body');
+const games = document.querySelector('.games');
 
-body.innerHTML += new GameThumbnail({
+games.innerHTML += new GameThumbnail({
 	name: 'Grand Theft Auto V',
 	background_image:
 		'https://media.rawg.io/media/games/84d/84da2ac3fdfc6507807a1808595afb12.jpg',
 	released: '2013-09-17',
+	metacritic: '97',
+}).render();
+
+games.innerHTML += new GameThumbnail({
+	name: 'Rocket League',
+	background_image:
+		'https://media.rawg.io/media/games/88c/88c5b4d7c80276c03ff62aebb1a99ad4.jpg',
+	released: '2019-09-17T03:06:45',
 	metacritic: '97',
 }).render();
